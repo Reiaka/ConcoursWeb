a0edc223f129eb25040fed22f0e18002a2bd8f95
Alice Mondja <liloumond@gmail.com>
2017-01-25 14:35:09 +0100

    Add files via upload

---
+++ creationAnimateur.php
@@ -0,0 +1,21 @@
+<html>
+	<head>
+	<title>Créer un évènement</title>	
+	</head>
+	<body>
+		<form method="POST" action="enregistrerAnim.php">
+	Titre: <input type="text" name="titre"/><br/>
+	Heure de Début : <input type="" name="heuredeb"/><br/>
+	Heure de Fin : <input type="" name="heurefin"/><br/>
+	Lieu : <input type="text" name="lieu"/><br/>
+	Date Limite : <input type= "" name="date"/><br/>
+	Type d'évènement:  <select name="event">
+				<option selected> Sélectionner le type d'évènement à créer</option>
+				<option> Conférence  </option>
+				<option> Atelier  </option>
+				<option> Visite  </option>
+			</select><br/>
+			<input type="submit" name="ok" value="Enregistrer">
+		</form>
+	</body>
+</html>
