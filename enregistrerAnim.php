f0ccdccc1f4cb42ee53cf8fc6be23ed84bedaee3
Alice Mondjanagni <liloumond@gmail.com>
2017-01-25 15:38:18 +0100

    On a try hard

--- enregistrerAnim.php
+++ enregistrerAnim.php
@@ -7,5 +7,6 @@
 	$connexion=mysqli_connect($host,$pseudo,$password) or die("Connexion impossible au serveur $host pour $pseudo");
 	mysqli_select_db($connexion,$bd)
 	$requete = "Insert into event values($_POST['titre'],$_POST['heuredeb'],$_POST['heurefin'],$_POST['lieu'],0)";
-	$resultat= mysqli_query(,$connexion,$req);
+	$resultat= mysqli_query(,$connexion,$requete);
+	header('Location: creationEvent.php');
 ?>
